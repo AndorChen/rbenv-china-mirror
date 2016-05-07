5ffef82876e255b8407cd6787cf2af3e3d603b00
Andor Chen <andor.chen.27@gmail.com>
2016-05-07 11:39:56 +0800

    changes the way to find definitions

--- etc/rbenv.d/install/china-mirror.bash
+++ etc/rbenv.d/install/china-mirror.bash
@@ -5,29 +5,39 @@ else
   echo "rbenv: rbenv-china-mirror plugin requires ruby-build 20130129 or later" >&2
 fi
 
+DEFINITION_PATH="${ARGUMENTS[0]}"
+
 change_to() {
+
   # skip build-in mirror
   export RUBY_BUILD_SKIP_MIRROR=1
 
-  local definition="${ARGUMENTS[0]}"
+  if [ -z "$DEFINITION_PATH" ]; then
+    usage 1 >&2
+  elif [ ! -f "$DEFINITION_PATH" ]; then
 
-  if [ -z "$definition" ]; then
-    usage
-  elif [ ! -f "$definition" ]; then
-    local definition_buildin_path="${RBENV_ROOT}/plugins/ruby-build/share/ruby-build/${definition}"
-    # is definition file exists?
-    if [ -e "$definition_buildin_path" ]; then
-      perl -p -i -e 's!cache.ruby-lang.org!cache.ruby-china.org!' $definition_buildin_path
-    else
-      echo "ruby-build: definition not found: ${definition_buildin_path}" >&2
+    OLDIFS="$IFS"
+    IFS=: RUBY_BUILD_DEFINITIONS=($RUBY_BUILD_DEFINITIONS ${RUBY_BUILD_ROOT:-$RUBY_BUILD_INSTALL_PREFIX}/share/ruby-build)
+
+    for DEFINITION_DIR in "${RUBY_BUILD_DEFINITIONS[@]}"; do
+      if [ -f "${DEFINITION_DIR}/${DEFINITION_PATH}" ]; then
+        DEFINITION_PATH="${DEFINITION_DIR}/${DEFINITION_PATH}"
+        perl -p -i -e 's!cache.ruby-lang.org!cache.ruby-china.org!' ${DEFINITION_PATH}
+        break
+      fi
+    done
+
+    IFS="$OLDIFS"
+
+    if [ ! -f "$DEFINITION_PATH" ]; then
+      echo "ruby-build: definition not found: ${DEFINITION_PATH}" >&2
       exit 2
     fi
   fi
 }
 
 change_back() {
-  if [ ! -f "$definition" ]; then
-    local definition_buildin_path="${RBENV_ROOT}/plugins/ruby-build/share/ruby-build/${ARGUMENTS[0]}"
-    perl -p -i -e 's!cache.ruby-china.org!cache.ruby-lang.org!' $definition_buildin_path
+  if [ ! -f "${ARGUMENTS[0]}" ]; then
+    perl -p -i -e 's!cache.ruby-china.org!cache.ruby-lang.org!' ${DEFINITION_PATH}
   fi
 }
